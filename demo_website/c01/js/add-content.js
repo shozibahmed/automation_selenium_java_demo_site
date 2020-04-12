f5010a3c14550212a3990e17b645383949ba67f9
Yusuf <yusufhamedny@Gmail.com>
2020-04-12 15:44:18 -0700

    update

---
+++ demo_website/c01/js/add-content.js
@@ -0,0 +1,19 @@
+
+var today = new Date();
+var hourNow=today.getHours();
+var greeting;
+if (hourNow>18){
+    greeting='Good evening!';
+}else if (hourNow>12){
+    greeting='Good Afternoon!'
+}else if (hourNow>0){
+    greeting='Good morning!'
+}else {
+    greeting='Welcome!';
+}
+
+document.write("<h3>"+greeting+"</h3>");
+
+var username="Nishat";
+var elName= document.getElementById("name");
+elName.textContent=username;
---
+++ demo_website/c01/script.js
@@ -0,0 +1,10 @@
+//Creating variables for the welcome message
+var greeting ='Howdy';
+var name='Molly';
+var message=', please check your order:';
+var welcome= greeting + name + message;
+
+var el = document.getElementById('greeting');
+el.textContent=welcome;
+
+
